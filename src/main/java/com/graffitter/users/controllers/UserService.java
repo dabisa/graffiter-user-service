8df3cb0c443fd80a7fed4ab32acc78ceabdde266
Dabisa Kelava <dabisa.kelava@gmail.com>
2022-04-19 23:12:35 +0100

    removed unused import

--- src/main/java/com/graffitter/users/controllers/UserService.java
+++ src/main/java/com/graffitter/users/controllers/UserService.java
@@ -37,7 +37,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @Service
